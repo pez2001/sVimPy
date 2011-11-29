bb0e41b2fe555bb73e55b1ad7fb103e9af725e2c
pez2001 <pez2001@voyagerproject.de>
2011-11-29 04:35:24 +0100

    added some more stuff

---
+++ src/unit_tests.c
@@ -0,0 +1,17 @@
+#include "unit_tests.h"
+
+void OpenPYC()
+{
+ printf("opening pyc file\n");
+ FILE *f;
+
+
+}
+
+
+int main(int argc,char *argv[])
+{
+ printf("Calling all Unit Tests\n");
+ OpenPYC();
+ return(0);
+}
---
+++ src/unit_tests.h
@@ -0,0 +1,10 @@
+#ifndef UNIT_TESTS_H
+#define UNIT_TESTS_H
+#include "vm.h"
+
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+#endif
