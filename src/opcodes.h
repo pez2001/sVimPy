833bc9ad3dbf384b5e7f43195e2eccae49429239
pez2001 <pez2001@voyagerproject.de>
2011-12-02 16:15:49 +0100

    changed debug paradigm

--- src/opcodes.h
+++ src/opcodes.h
@@ -3,21 +3,29 @@
 
 #define DEBUG
 
-#ifdef DEBUG
 
 typedef struct {
 unsigned char opcode;  //opcode in bytecode representation
+#ifdef DEBUG
 char *name;    //string version
 char *description;    //a short description
+#endif
 unsigned int num_parameters; //number of parameters
 } opcode;
 
+#ifdef DEBUG
 instruction instructions[]={{0x00,"STOP_CODE","Indicates end-of-code to the compiler, not used by the interpreter.",0},
                             {0x01,"POP_TOP","Removes the top-of-stack (TOS) item.",0},
                             {0x,"","",0}
                             };
-
+#elseif
+instruction instructions[]={{0x00,0},
+                            {0x01,0},
+                            {0x,0}
+                            };
 
 #endif
+
+
 
 #endif
