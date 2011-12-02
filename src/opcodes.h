4b3c8c305c37d0b986db5ec1571983e9cba97a79
pez2001 <pez2001@voyagerproject.de>
2011-12-02 16:12:09 +0100

    added opcodes struct for debugging

--- src/opcodes.h
+++ src/opcodes.h
@@ -1,5 +1,23 @@
 #ifndef OPCODES_H
 #define OPCODES_H
 
+#define DEBUG
+
+#ifdef DEBUG
+
+typedef struct {
+unsigned char opcode;  //opcode in bytecode representation
+char *name;    //string version
+char *description;    //a short description
+unsigned int num_parameters; //number of parameters
+} opcode;
+
+instruction instructions[]={{0x00,"STOP_CODE","Indicates end-of-code to the compiler, not used by the interpreter.",0},
+                            {0x01,"POP_TOP","Removes the top-of-stack (TOS) item.",0},
+                            {0x,"","",0}
+                            };
+
+
+#endif
 
 #endif
